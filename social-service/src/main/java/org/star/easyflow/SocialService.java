62176df42ac7e2615dd8a7d3819f2d9386bc1a3c
Vladislav <unmei.vs@gmail.com>
2024-05-04 03:20:14 +0600

    rename services

--- social-service/src/main/java/org/star/easyflow/SocialService.java
+++ social-service/src/main/java/org/star/easyflow/SocialService.java
@@ -6,8 +6,8 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @EnableFeignClients
 @SpringBootApplication
-class Easyflow {
+class SocialService {
     public static void main(String[] args) {
-        SpringApplication.run(Easyflow.class, args);
+        SpringApplication.run(SocialService.class, args);
     }
 }
